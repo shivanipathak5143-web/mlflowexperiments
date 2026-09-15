a4d1b18f547af16061deb6f7093cd9c74603aa41
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-15 18:55:16 +0530

    App.py changes

--- app.py
+++ app.py
@@ -73,6 +73,11 @@ if __name__ == "__main__":
 
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
+        ##for Remote server only(DAGShub)
+        remote_server_uri="https://dagshub.com/shivanipathak5143/mlflowexperiments.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
+        
+
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         ## For Remote server only(DAGShub)
